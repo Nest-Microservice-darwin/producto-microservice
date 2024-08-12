f09eaefc416da70d7e2925c941c659289ddb4483
DARWIN UZCATEGUI <darwin.uzcategui1973@hotmail.com>
2024-08-12 11:50:52 -0400

    cambio producto exep

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
