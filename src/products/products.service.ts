2886471243a56195b3d650c6e32668dfdd27d0d2
DARWIN UZCATEGUI <darwin.uzcategui1973@hotmail.com>
2024-08-12 12:24:07 -0400

    CAMBIO PUNTUAL BAD REQUIEST

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -63,7 +63,11 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
      if (!product) {
       // se puede manejar este  Exception throw new NotFoundException(`Product with  id #${id} not found`)
-      throw new RpcException(`Product with  id #${id} not found`)
+      //throw new RpcException(`Product with  id #${id} not found`)
+      throw new RpcException({ 
+        message :`Product with  id #${id} not found`,
+        status : HttpStatus.BAD_REQUEST
+      })
      }
 
      return product;
